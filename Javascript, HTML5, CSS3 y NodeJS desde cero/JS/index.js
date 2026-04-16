41e15fa2c43ad6787eaf086f1bae2e4067aee308
Antonio <antoniomrm21@gmail.com>
2026-04-16 11:53:25 +0200

    JS, declaración de variables y tipos de datos

---
+++ Javascript, HTML5, CSS3 y NodeJS desde cero/JS/index.js
@@ -0,0 +1,33 @@
+console.log('Hola Mundo')
+
+// var miPrimeraVariable = 'HOLA'
+
+// let miPrimeraVariable ='Estoy loco'
+// console.log(miPrimeraVariable)
+
+// miPrimeraVariable = 'Esto ha cambiado'
+// console.log(miPrimeraVariable)
+
+// const miPrimerObjeto = {}
+
+const miObjeto = {
+    unNumero: 21,
+    unString: 'Esta cadena',
+    unaCondicion: true,
+}
+
+// console.log(miObjeto)
+
+const arrayVacio = []
+const array = [1,2,'Hola',miObjeto]
+
+console.log(array)
+
+array.push(5)
+arrayVacio.push(5)
+arrayVacio.push(5)
+arrayVacio.push(5)
+arrayVacio.push(5)
+arrayVacio.push(5)
+
+console.log(arrayVacio)
