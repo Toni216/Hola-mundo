4dbaf3d8cdd76dacf1e3392d5d92ab8af692357d
Antonio <antoniomrm21@gmail.com>
2026-04-16 12:37:18 +0200

    JS Control de flujo

--- Javascript, HTML5, CSS3 y NodeJS desde cero/JS/index.js
+++ Javascript, HTML5, CSS3 y NodeJS desde cero/JS/index.js
@@ -1,15 +1,33 @@
-const resultado1 = 5 < 6
-const resultado2 = 5 > 5
-const resultado3 = 5 > 6
-const resultado4 = 5 > 4
-const resultado5 = 5 <= 5
-const resultado6 = 5 <= 6
-const resultado7 = 5 >= 5
-const resultado8 = 5 >= 6
-const resultado9 = 5 !== '5'
-const resultado10 = 5 != '5'
+// const edad = 10
+// if (edad > 5 && edad < 18) {
+//     console.log('Estoy loco')
+// }
 
-console.log(resultado9, resultado10)
+// let x= 0
+// while(x < 5) {
+//     console.log(x)
+//     x++
+// }
 
-const resultadoOr = false || true 
-console.log(resultadoOr)
+// switch('minecraft si joder') {
+//     case 1: 
+//         console.log('yo soy el caso 1')
+//         break
+//     case 2:
+//         console.log('estoy loco')
+//         break
+//     case 3:
+//         console.log('estoy feliz')
+//         break
+//     default:
+//         console.log('amo a mi novia')
+//         break
+// }
+
+const numeros = [1,2,3,4,5]
+
+// console.log(numeros[2]);
+
+for(let i = 0; i < numeros.length; i++) {
+    console.log(numeros[i])
+}
